5f431ea3b57283740b5546a7298f35e2c7c29d5d
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2024-12-04 00:52:42 +0100

    Added enum

---
+++ Ace7LocalizationFormat/Enums/eLanguage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ace7LocalizationFormat.Enums
+{
+    public enum eLanguage : int
+    {
+        ENGLISH = 65,
+        TRADITIONAL_CHINESE = 66,
+        FRENCH = 67,
+        GERMAN = 68,
+        ITALIAN = 69,
+        JAPANESE = 70,
+        KOREAN = 71,
+        EUROPEAN_SPANISH = 72,
+        LATIN_AMERICAN_SPANISH = 73,
+        POLISH = 74,
+        BRAZILIAN_PORTUGUESE = 75,
+        RUSSIAN = 76,
+        SIMPLIFIED_CHINESE = 77,
+    }
+}
